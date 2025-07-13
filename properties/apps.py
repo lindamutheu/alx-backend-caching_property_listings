354f80651aebdbbc4b0dd611a4c7221d34e1d14f
Alpha Infinos <kevinmullerhi@gmail.com>
2025-07-13 13:09:33 +0300

    initial commit

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,12 @@
-from django.apps import AppConfig
+#!/usr/bin/env python3
+"""AppConfig for properties app"""
 
+from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+    def ready(self):
+        """Import signals to connect them to model events"""
+        import properties.signals
